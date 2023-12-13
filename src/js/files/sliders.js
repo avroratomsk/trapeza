a3af63237003d48410ac943cc9fb33506c9a3f0f
avroratomsk <avroratomsk@yandex.ru>
2023-12-13 09:16:30 +0700

    burger-menu

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -29,7 +29,6 @@ function initSliders() {
   // Перечень слайдеров
   // Проверяем, есть ли слайдер на стронице
   
-
   if (document.querySelector('.index-slider__slider')) { // Указываем скласс нужного слайдера
     // Создаем слайдер
     new Swiper('.index-slider__slider', { // Указываем скласс нужного слайдера
@@ -76,6 +75,87 @@ function initSliders() {
       navigation: {
         prevEl: '.index-button-prev',
         nextEl: '.index-button-next',
+      },
+
+      // navigation: {
+      //   prevEl: '.swiper-button-prev',
+      //   nextEl: '.swiper-button-next',
+      // },
+
+      // Брейкпоинты
+      // breakpoints: {
+      // 	320: {
+      // 		slidesPerView: 1,
+      // 		spaceBetween: 20,
+      // 		autoHeight: true,
+      // 	},
+      // 	485: {
+      // 		slidesPerView: 2,
+      // 		spaceBetween: 20,
+      // 	},
+      // 	992: {
+      // 		slidesPerView: 3,
+      // 		spaceBetween: 20,
+      // 	},
+      // 	1268: {
+      // 		slidesPerView: 3,
+      // 		spaceBetween: 30,
+      // 	},
+      // },
+      // События
+      on: {
+
+      }
+    });
+  }
+
+  if (document.querySelector('.reviews__slider')) { // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.reviews__slider', { // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination, Autoplay],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 3,
+      spaceBetween: 35,
+      autoHeight: true,
+      centeredSlides: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+
+      // Эффекты
+      // effect: 'fade',
+      autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+      },
+
+      // Пагинация
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+      },
+
+      // Скроллбар
+      /*
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+      */
+
+      // Кнопки "влево/вправо"
+      navigation: {
+        prevEl: '.reviews__controls-prev',
+        nextEl: '.reviews__controls-next',
       },
 
       // navigation: {
