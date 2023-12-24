6e81cdff89b7ec734d3332226c37b2e99ca322de
avroratomsk <avroratomsk@yandex.ru>
2023-12-24 22:39:50 +0700

    feat: deploy prod

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -187,6 +187,86 @@ function initSliders() {
       on: {},
     });
   }
+
+  if (document.querySelector(".days__slider")) {
+    // Указываем скласс нужного слайдера
+    // Создаем слайдер
+    new Swiper(".days__slider", {
+      // Указываем скласс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination, Autoplay],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 7,
+      spaceBetween: 35,
+      autoHeight: true,
+      centeredSlides: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      //preloadImages: false,
+      //lazy: true,
+
+      // Эффекты
+      // effect: 'fade',
+      autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+      },
+
+      // Пагинация
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+
+      // Скроллбар
+      /*
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+      */
+
+      // Кнопки "влево/вправо"
+      navigation: {
+        prevEl: ".reviews__controls-prev",
+        nextEl: ".reviews__controls-next",
+      },
+
+      // navigation: {
+      //   prevEl: '.swiper-button-prev',
+      //   nextEl: '.swiper-button-next',
+      // },
+
+      // Брейкпоинты
+      // breakpoints: {
+      // 	320: {
+      // 		slidesPerView: 1,
+      // 		spaceBetween: 20,
+      // 		autoHeight: true,
+      // 	},
+      // 	485: {
+      // 		slidesPerView: 2,
+      // 		spaceBetween: 20,
+      // 	},
+      // 	992: {
+      // 		slidesPerView: 3,
+      // 		spaceBetween: 20,
+      // 	},
+      // 	1268: {
+      // 		slidesPerView: 3,
+      // 		spaceBetween: 30,
+      // 	},
+      // },
+      // События
+      on: {},
+    });
+  }
 }
 
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
