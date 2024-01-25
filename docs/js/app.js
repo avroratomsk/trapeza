c4c74a0cf20d02d21e41c43cca4fc04f02bbebce
avroratomsk <avroratomsk@yandex.ru>
2024-01-25 20:56:14 +0700

    develop single page

--- docs/js/app.js
+++ docs/js/app.js
@@ -4699,71 +4699,17 @@
     new VenoBox({
         selector: ".index-gallery__item"
     });
-    const anchorLink = document.querySelectorAll("[data-anchor]");
-    if (anchorLink) anchorLink.forEach((item => {
-        item.addEventListener("click", (function(e) {
-            let dataAttrDate = item.dataset.anchor;
-            let section = document.getElementById(dataAttrDate);
-            let topBorderSection = section.getBoundingClientRect().top;
-            let headerHeight = document.querySelector("header").clientHeight;
-            let scrollHeight = topBorderSection - headerHeight;
-            window.scrollBy({
-                top: scrollHeight,
-                left: 0,
-                behavior: "smooth"
-            });
-        }));
+    const openSearchBtn = document.getElementById("open-search");
+    if (openSearchBtn) openSearchBtn.addEventListener("click", (function(e) {
+        this.classList.toggle("_active");
+        document.getElementById("single-search").classList.toggle("_active");
+        if (!this.classList.contains("_active")) this.innerHTML = '<i class="fa-solid fa-magnifying-glass"></i>'; else this.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     }));
-    const btnBurger = document.querySelector(".header__burger");
-    if (btnBurger) btnBurger.addEventListener("click", (function(e) {
-        scrollLock();
-        document.documentElement.classList.add("lock");
-        btnBurger.classList.add("active");
-        document.querySelector(".popup-menu").classList.add("show");
-    }));
-    const closeMenuPopup = document.querySelector(".popup-menu__close");
-    if (closeMenuPopup) closeMenuPopup.addEventListener("click", (function(e) {
-        document.documentElement.classList.remove("lock");
-        scrollUnlock();
-        btnBurger.classList.remove("active");
-        document.querySelector(".popup-menu").classList.remove("show");
-    }));
-    function scrollLock() {
-        let scrollWidth = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-        document.querySelector("body").style.paddingRight = scrollWidth;
-        document.querySelector(".header").style.paddingRight = scrollWidth;
-    }
-    function scrollUnlock() {
-        window.innerWidth, document.querySelector(".wrapper").offsetWidth;
-        document.querySelector("body").style.paddingRight = "0px";
-        document.querySelector(".header").style.paddingRight = "0px";
-    }
-    let headerLogo = document.querySelector(".header__logo");
-    headerLogo.getBoundingClientRect();
-    headerLogo.getBoundingClientRect().left;
-    let zoomBtn = document.querySelectorAll(".item-list__zoombtn");
-    if (zoomBtn) zoomBtn.forEach((btn => {
-        btn.addEventListener("click", (function(e) {
-            let parent = btn.closest(".item-list");
-            if (parent) {
-                let image = parent.querySelector(".item-list-image").src;
-                let title = parent.querySelector(".item-list__name").innerText;
-                let info = parent.querySelector(".item-list__charact").innerText;
-                let bjy = parent.querySelector(".item-list__bjy").innerText;
-                let text = parent.querySelector(".item-list__descr").innerHTML;
-                let popup = document.querySelector(".popup-fulldescr");
-                popup.querySelector(".popup-image").src = image;
-                popup.querySelector(".popup__title").innerText = title;
-                popup.querySelector(".popup__price").innerText = info;
-                popup.querySelector(".popup__bjy").innerText = bjy;
-                popup.querySelector(".popup__descr").innerHTML = text;
-            }
-        }));
-    }));
-    let menuBtn = document.querySelector(".menu-btn");
-    if (menuBtn) menuBtn.addEventListener("click", (function(e) {
-        this.classList.toggle("menu-btn_active");
-        document.querySelector(".menu").classList.toggle("menu_active");
+    const openFilterBtn = document.getElementById("open-filter");
+    if (openFilterBtn) openFilterBtn.addEventListener("click", (function(e) {
+        this.classList.toggle("_active");
+        document.getElementById("filter-sort").classList.toggle("_active");
+        if (!this.classList.contains("_active")) this.innerHTML = '<i class="fa-solid fa-sliders"></i>'; else this.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     }));
     window["FLS"] = true;
     isWebp();
